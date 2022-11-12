cad4b156af688859fe3f9aaca1df864468d2132d
Vinnicius Cardoso <vinnicius.g.cardoso@gmail.com>
2022-11-12 13:41:26 -0400

    Projeto Tensorflow
    Foi arrumado as pastas do projeto realizado no modulo de NN e DP com TensorFlow

---
+++ RedesNeurais_DeepLearning_TensorFlow/Projeto/Bancoimagens/teste.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+from keras.callbacks import ReduceLROnPlateau
+import cv2
+import os
+import tensorflow as tf
+import numpy as np
+
+labels = ['PNEUMONIA', 'NORMAL']
+img_size = 150
+def get_training_data(data_dir):
+    data = []
+    for label in labels:
+        path = os.path.join(data_dir, label) # junta o diretoria + a "label"
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+train = get_training_data('train')
+test = get_training_data('test')
+val = get_training_data('val')
+
+x_train = []
+y_train = []
+
+x_val = []
+y_val = []
+
+x_test = []
+y_test = []
+
+for feature, label in train:
+    x_train.append(feature)
+    y_train.append(label)
+
+for feature, label in test:
+    x_test.append(feature)
+    y_test.append(label)
+
+for feature, label in val:
+    x_val.append(feature)
+    y_val.append(label)
+
+x_train = np.array(x_train) / 255
+x_val = np.array(x_val) / 255
+x_test = np.array(x_test) / 255
+
+# resize data for deep learning
+x_train = x_train.reshape(-1, img_size, img_size, 1)
+y_train = np.array(y_train)
+
+x_val = x_val.reshape(-1, img_size, img_size, 1)
+y_val = np.array(y_val)
+
+x_test = x_test.reshape(-1, img_size, img_size, 1)
+y_test = np.array(y_test)
+
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
+    zoom_range = 0.2, # Randomly zoom image
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip = True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
+
+
+
+datagen.fit(x_train)
